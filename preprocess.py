7d32a1642a812b0cbdaf255cedc6e8cf69990a54
1144746221 <weiwang.ysu@gmail.com>
2019-12-31 16:44:21 +0800

    rebulid vocab

--- preprocess.py
+++ preprocess.py
@@ -1,101 +1,40 @@
-import jieba
-import json
+import argparse
 import os
+import json
 
 
-def process_sample(line):
-    term = line.strip().split('\t')
-    if len(term) != 9:
-        return None
-    url = term[0]
-    tags, topic = process_tags(term[4])
-    for tag in tags:
-        jieba.add_word(tag[0])  # tag: (tag, value)
-    # title = list(jieba.cut(term[1]))
-    title = term[1]
-    # abstract = list(jieba.cut(term[2]))
-    abstract = term[2]
-    # comments = process_comments(term[8])
-    comments = term[8]
-    return url, title, abstract, tags, topic, comments
+def parse_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-data', default='data_path', type=str, help="config file")
+    return parser.parse_args()
 
 
-def process_tags(tags):
-    terms = tags.split('$')[:-1]  # line ends with $
-    tags = []
-    topics = []
-    for term in terms:
-        tem = term.split(':')
-        if len(tem) != 3:
-            continue
-        tag, type, value = tem
-        if float(value) >= 0 and type == 'tag':
-            tags.append((tag, float(value)))
-        elif type == 'topic':
-            topics.append(tag)
-    topic = None
-    if len(topics) > 0:
-        topic = topics[0]
-        for i in range(len(topics)):
-            if len(topics[i]) > len(topic):
-                topic = topics[i]
-        topic = topic.split('_')
-    return tags, topic
+def build_vocab(corpus_files, vocab_file):
+    word2count = {}
+    for corpus_file in corpus_files:
+        for line in open(corpus_file):
+            # words = line.strip().split()
+            g = json.loads(line)
+            words = g["body"].split()
+            words.extend(g["title"].split())
+            words.extend([w for com in g["comment"] for w in com[0].split()])
+            for word in words:
+                if word not in word2count:
+                    word2count[word] = 0
+                word2count[word] += 1
+    word2count = list(word2count.items())
+    word2count.sort(key=lambda k: k[1], reverse=True)
+    write = open(vocab_file, 'w')
+    for word_pair in word2count:
+        write.write(word_pair[0] + '\t' + str(word_pair[1]) + '\n')
+    write.close()
 
 
-def process_comments(comments):
-    terms = comments.split('$$')  # line ends with $$
-    comments = []
-    for term in terms:
-        tem = term.split('::')
-        if len(tem) != 2:
-            print(tem)
-            continue
-        comment, value = tem
-        try:
-            value = float(value)
-        except ValueError:
-            print(tem)
-            tem = term.split(':::')
-            if len(tem) != 2:
-                print(tem)
-                continue
-            comment, value = tem
-        comment_words = list(jieba.cut(comment))
-        try:
-            value = float(value)
-        except ValueError:
-            print(term)
-            continue
-        comments.append((comment_words, value))
-    return comments
-
-
-def read_file(fname, topic_limit=[u'娱乐']):
-    data = []
-    extend_num = 0
-    share_num = 0
-    topic_num = 0
-    for line in open(fname, encoding='utf-8'):
-        tem = process_sample(line)
-        if tem is None:
-            continue
-        url, title, abstract, tags, topic, comments = tem
-        if topic is not None and topic[0] in topic_limit:
-            topic_num += 1
-            for comment in comments:
-                real_tags = {t[0] for t in tags}
-                share = len(real_tags.intersection(set(comment[0])))
-                if share > 0:
-                    share_num += 1
-                extend = has_entity(comment[0])
-                if extend:
-                    extend_num += 1
-                if share > 0 or extend or comment[1] > 1:  # comment[1] is voting number
-                    data.append((url, title, abstract, tags, topic, comment))
-    print('extend num', extend_num, 'share_num', share_num, 'topic_num', topic_num)
-    json.dump(data, open('toutiao_data.json', 'w'), ensure_ascii=False, indent=4)
-
-
-if __name__ == '__main__':
-    read_file('./data/toutiao.tsv')
+if __name__ == "__main__":
+    args = parse_config()
+    data_path = args.data
+    # corpus_files = ['data.train.json']
+    corpus_files = ['data.train.json', 'data.dev.json', 'data.test.json']
+    corpus_files = [os.path.join(data_path, split) for split in corpus_files]
+    vocab_file = os.path.join(data_path, 'vocab.txt')
+    build_vocab(corpus_files, vocab_file)
