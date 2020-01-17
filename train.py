804313664adfcaa33a18ac461706b4fd4371dfb9
1144746221 <weiwang.ysu@gmail.com>
2020-01-17 13:22:53 +0800

    test4

--- train.py
+++ train.py
@@ -39,7 +39,8 @@ def parse_args():
                                  'select2seq', 'select_var_diverse2seq',
                                  'var_select_var_diverse2seq', 'var_select_var_user_diverse2seq',
                                  'select2seq_test', 'var_select_var_user_diverse2seq_test',
-                                 'var_select_var_user_diverse2seq_test2', 'var_select_var_user_diverse2seq_test3'])
+                                 'var_select_var_user_diverse2seq_test2', 'var_select_var_user_diverse2seq_test3',
+                                 'var_select_var_user_diverse2seq_test4'])
     parser.add_argument('-adj', type=str, default="numsent",
                         help='adjacent matrix')
     parser.add_argument('-use_copy', default=False, action="store_true",
@@ -351,6 +352,8 @@ def main():
         model = var_select_var_user_diverse2seq_test2(config, vocab, use_cuda)
     elif args.model == 'var_select_var_user_diverse2seq_test3':
         model = var_select_var_user_diverse2seq_test3(config, vocab, use_cuda)
+    elif args.model == 'var_select_var_user_diverse2seq_test4':
+        model = var_select_var_user_diverse2seq_test4(config, vocab, use_cuda)
 
     # total number of parameters
     logging(repr(model) + "\n\n")
